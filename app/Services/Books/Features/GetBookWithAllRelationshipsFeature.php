8d223cc3052a7765e06019f9ae46e36d4441d6c0
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-08-20 02:19:53 +0400

    + get book with all feature with test body + use presenter strategy to decide proper response on runtime

---
+++ app/Services/Books/Features/GetBookWithAllRelationshipsFeature.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services\Books\Features;
+
+use App\Contracts\Presenter\CreatesConcretePresenter;
+use App\Contracts\Presenter\PresentsViaPresenterStrategy;
+use App\Domains\Book\Jobs\GetBookWithAllRelationshipsJob;
+use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Lucid\Units\Feature;
+
+class GetBookWithAllRelationshipsFeature extends Feature
+{
+    use CreatesConcretePresenter,
+        PresentsViaPresenterStrategy;
+
+    const ERROR_MESSAGE_NOT_FOUND = 'The requested book does not exist.';
+
+    public function __construct(
+        private readonly GetBookWithAllRelationshipsJob $getBookWithAllRelationshipsJob
+    )
+    {
+    }
+
+    public function handle(Request $request, int $bookId): View|Application|Factory|ApplicationContract|Response
+    {
+        $presenter = $this->getConcretePresenter($request);
+        $book = $this->getBookWithAllRelationshipsJob->handle($bookId);
+        if (!$book) {
+            throw $presenter->presentNotFoundError(
+                extra: null,message: self::ERROR_MESSAGE_NOT_FOUND
+            );
+        }
+
+        return $this->present(
+            presenter: $presenter,
+            data: $book
+        );
+    }
+}
---
+++ tests/Feature/Services/Books/GetBookWithAllRelationshipsFeatureTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature\Services\Books;
+
+use Tests\TestCase;
+use App\Services\Books\Features\GetBookWithAllRelationshipsFeature;
+
+class GetBookWithAllRelationshipsFeatureTest extends TestCase
+{
+    public function test_get_book_with_all_relationships_feature()
+    {
+        $this->markTestIncomplete();
+    }
+}
