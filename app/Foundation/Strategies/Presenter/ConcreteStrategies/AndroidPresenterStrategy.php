16dad8a751551806e291c33c03e06fefe02d57a6
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-08-20 02:48:29 +0400

    -+ conform to changes in android strategy

--- app/Foundation/Strategies/Presenter/ConcreteStrategies/AndroidPresenterStrategy.php
+++ app/Foundation/Strategies/Presenter/ConcreteStrategies/AndroidPresenterStrategy.php
@@ -3,14 +3,14 @@
 namespace App\Foundation\Strategies\Presenter\ConcreteStrategies;
 
 use App\Foundation\Composables\HttpExceptions\ThrowsErrorForAndroid;
-use App\Foundation\Composables\Responses\SendsResponseForAndroid;
+use App\Foundation\Composables\Responses\SendsResponseForAndroidAgent;
 use App\Foundation\Strategies\Presenter\PresenterStrategyInterface;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\Response;
 
 class AndroidPresenterStrategy implements PresenterStrategyInterface
 {
-    use SendsResponseForAndroid,
+    use SendsResponseForAndroidAgent,
         ThrowsErrorForAndroid;
 
     public function presentData(mixed $data): Response
